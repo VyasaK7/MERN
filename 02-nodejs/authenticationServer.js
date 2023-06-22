778174aa173a382865dcfa9fb4d224321d53a272
Vyasa Krishna K <vyasak253@gmail.com>
2023-06-22 13:27:22 +0530

    Completed Writing Servers

--- 02-nodejs/authenticationServer.js
+++ 02-nodejs/authenticationServer.js
@@ -1,37 +1,86 @@
-/**
-  You need to create a HTTP server in Node.js which will handle the logic of an authentication server.
-  - Don't need to use any database to store the data.
-
-  - Save the users and their signup/login data in an array in a variable
-  - You can store the passwords in plain text (as is) in the variable for now
-
-  The expected API endpoints are defined below,
-  1. POST /signup - User Signup
-    Description: Allows users to create an account. This should be stored in an array on the server, and a unique id should be generated for every new user that is added.
-    Request Body: JSON object with username, password, firstName and lastName fields.
-    Response: 201 Created if successful, or 400 Bad Request if the username already exists.
-    Example: POST http://localhost:3000/signup
-
-  2. POST /login - User Login
-    Description: Gets user back their details like firstname, lastname and id
-    Request Body: JSON object with username and password fields.
-    Response: 200 OK with an authentication token in JSON format if successful, or 401 Unauthorized if the credentials are invalid.
-    Example: POST http://localhost:3000/login
-
-  3. GET /data - Fetch all user's names and ids from the server (Protected route)
-    Description: Gets details of all users like firstname, lastname and id in an array format. Returned object should have a key called users which contains the list of all users with their email/firstname/lastname.
-    The users username and password should be fetched from the headers and checked before the array is returned
-    Response: 200 OK with the protected data in JSON format if the username and password in headers are valid, or 401 Unauthorized if the username and password are missing or invalid.
-    Example: GET http://localhost:3000/data
-
-  - For any other route not defined in the server return 404
-
-  Testing the server - run `npm run test-authenticationServer` command in terminal
- */
-
-const express = require("express")
-const PORT = 3000;
+const express = require('express');
 const app = express();
-// write your logic here, DONT WRITE app.listen(3000) when you're running tests, the tests will automatically start the server
+const port = 3000;
 
-module.exports = app;
+const users = [];
+
+app.use(express.json());
+
+const isUsernameTaken = (username) => {
+  return users.some(user => user.username === username);
+};
+
+const findUser = (username, password) => {
+  return users.find(user => user.username === username && user.password === password);
+};
+
+app.post('/signup', (req, res) => {
+  const { username, password, firstName, lastName } = req.body;
+  
+  if (isUsernameTaken(username)) {
+    res.status(400).send('Username already exists');
+  } else {
+    const id = users.length + 1;
+    const newUser = {
+      id,
+      username,
+      password,
+      firstName,
+      lastName
+    };
+    users.push(newUser);
+    res.status(201).send('User created');
+  }
+});
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  const user = findUser(username, password);
+
+  if (user) {
+    const token = generateAuthToken();
+
+    res.status(200).json({
+      id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      token
+    });
+
+  } else {
+    res.status(401).send('Invalid credentials');
+  }
+});
+
+app.get('/data', (req, res) => {
+  const username = req.headers.username;
+  const password = req.headers.password;
+  const user = findUser(username, password);
+
+  if (user) {
+    const userData = users.map(({ id, firstName, lastName }) => ({
+      id,
+      firstName,
+      lastName
+    }));
+
+    res.status(200).json({
+      users: userData
+    });
+  } else {
+    res.status(401).send('Invalid credentials');
+  }
+});
+
+app.use((req, res) => {
+  res.status(404).send('Not Found');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+
+function generateAuthToken() {
+  return 'vyasa-auth-token';
+}
